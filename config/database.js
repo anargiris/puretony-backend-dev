c8d59d0760ce04b050601780282439e4d6a02d89
anargiris <anargiriss@hotmail.com>
2021-05-02 14:58:55 +0300

    testing

--- config/database.js
+++ config/database.js
@@ -4,12 +4,6 @@ module.exports = ({ env }) => ({
     default: {
       connector: "mongoose",
       settings: {
-        host: env("DATABASE_HOST", "@devfind.88umm.mongodb.net"),
-        srv: env.bool("DATABASE_SRV", true),
-        port: env.int("DATABASE_PORT", 27017),
-        database: env("DATABASE_NAME", "puretony-backend"),
-        username: env("DATABASE_USERNAME", "anargiris"),
-        password: env("DATABASE_PASSWORD", "petetownshend20"),
         uri: env("DATABASE_URI"),
       },
       options: {
