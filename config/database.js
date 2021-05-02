887a18bf1da39d07718e500a64e9c4331eebb342
anargiris <anargiriss@hotmail.com>
2021-05-02 14:41:11 +0300

    Update database config

--- config/database.js
+++ config/database.js
@@ -1,19 +1,20 @@
 module.exports = ({ env }) => ({
-  defaultConnection: 'default',
+  defaultConnection: "default",
   connections: {
     default: {
-      connector: 'mongoose',
+      connector: "mongoose",
       settings: {
-        host: env('DATABASE_HOST', '@devfind.88umm.mongodb.net'),
-        srv: env.bool('DATABASE_SRV', true),
-        port: env.int('DATABASE_PORT', 27017),
-        database: env('DATABASE_NAME', 'puretony-backend'),
-        username: env('DATABASE_USERNAME', 'anargiris'),
-        password: env('DATABASE_PASSWORD', 'petetownshend20'),
+        host: env("DATABASE_HOST", "@devfind.88umm.mongodb.net"),
+        srv: env.bool("DATABASE_SRV", true),
+        port: env.int("DATABASE_PORT", 27017),
+        database: env("DATABASE_NAME", "puretony-backend"),
+        username: env("DATABASE_USERNAME", "anargiris"),
+        password: env("DATABASE_PASSWORD", "petetownshend20"),
+        uri: env("DATABASE_URI"),
       },
       options: {
-        authenticationDatabase: env('AUTHENTICATION_DATABASE', null),
-        ssl: env.bool('DATABASE_SSL', true),
+        authenticationDatabase: env("AUTHENTICATION_DATABASE", null),
+        ssl: env.bool("DATABASE_SSL", true),
       },
     },
   },
